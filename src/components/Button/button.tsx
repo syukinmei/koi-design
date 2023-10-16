f28565094055401cfd4cb9c6b57b529eba951253
syukinmei <syukinmei321@163.com>
2023-10-16 12:29:03 +0800

    feat:Button组件添加onClick属性、将loading逻辑内置
    loading效果还未处理

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import classNames from "classnames";
 
 interface BaseButtonProps {
@@ -19,19 +19,38 @@ interface BaseButtonProps {
    */
   type?: "primary" | "link" | "danger" | "default";
   /**
+   * 点击事件
+   */
+  onClick?: (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => void | Promise<any>;
+  /**
    * 子元素
    */
   children: React.ReactNode;
 }
 export const Button: React.FC<BaseButtonProps> = (props) => {
-  const { className, disabled, size, type, children } = props;
+  const { className, disabled, size, type, onClick, children } = props;
+
+  const [loading, setLoading] = useState<boolean>(false);
+
   const classes = classNames("btn", className, {
     [`btn-type-${type}`]: type,
     [`btn-size-${size}`]: size,
   });
+
+  const handleClick = async (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    if (!onClick || loading) return;
+    setLoading(true);
+    await onClick(event);
+    setLoading(false);
+  };
+
   return (
-    <button className={classes} disabled={disabled}>
-      {children}
+    <button className={classes} disabled={disabled} onClick={handleClick}>
+      {loading ? "加载中..." : children}
     </button>
   );
 };
